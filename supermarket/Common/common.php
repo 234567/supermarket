ed71eddff5ec08c1ebebe3c17673138302e742b9
googlerobot <googlerobot@c72bb6ee-9a7c-4492-ae2e-2bf3b3448c13>
2013-05-06 13:30:04 +0000

    完善日期格式化。
    git-svn-id: svn://localhost/market/trunk@82 c72bb6ee-9a7c-4492-ae2e-2bf3b3448c13

--- supermarket/Common/common.php
+++ supermarket/Common/common.php
@@ -32,24 +32,45 @@ function mdate($time = NULL) {
         return '';
     }
     $text = '';
-    $time = $time === NULL || $time > time() ? time() : intval($time);
-    $t = time() - $time; //时间差 （秒）
-    if ($t == 0)
-        $text = '刚刚';
-    elseif ($t < 60)
-        $text = $t . '秒前'; // 一分钟内
-    elseif ($t < 60 * 60)
-        $text = floor($t / 60) . '分钟前'; //一小时内
-    elseif ($t < 60 * 60 * 24)
-        $text = floor($t / (60 * 60)) . '小时前'; // 一天内
-    elseif ($t < 60 * 60 * 24 * 3)
-        $text = floor($t/(60*60*24)) ==1? '昨天 ' . date('H:i', $time) : '前天 ' . date('H:i', $time) ; //昨天和前天
-    elseif ($t < 60 * 60 * 24 * 30)
-        $text = date('m月d日 H:i', $time); //一个月内
-    elseif ($t < 60 * 60 * 24 * 365)
-        $text = date('m月d日', $time); //一年内
-    else
-        $text = date('Y年m月d日', $time); //一年以前
+    $now = time();
+    $time = $time === NULL ? $now : intval($time);
+    if($time <= $now){
+        $t = $now - $time; //时间差 （秒）
+        if ($t == 0)
+            $text = '刚刚';
+        elseif ($t < 60)
+            $text = $t . '秒前'; // 一分钟内
+        elseif ($t < 60 * 60)
+            $text = floor($t / 60) . '分钟前'; //一小时内
+        elseif ($t < 60 * 60 * 24)
+            $text = floor($t / (60 * 60)) . '小时前'; // 一天内
+        elseif ($t < 60 * 60 * 24 * 3)
+            $text = floor($t/(60*60*24)) ==1? '昨天 ' . date('H:i', $time) : '前天 ' . date('H:i', $time) ; //昨天和前天
+        elseif ($t < 60 * 60 * 24 * 30)
+            $text = date('m月d日 H:i', $time); //一个月内
+        elseif ($t < 60 * 60 * 24 * 365)
+            $text = date('m月d日', $time); //一年内
+        else
+            $text = date('Y年m月d日', $time); //一年以前
+    }else{
+        $t = $time - $now; //时间差 （秒）
+        if ($t < 60)
+            $text = $t . '秒后';
+        elseif ($t < 60 * 60)
+            $text = floor($t / 60) . '分钟'.floor($t % 60).'秒后';
+        elseif ($t < 60 * 60 * 24)
+            $text = floor($t / (60 * 60)) . '小时'.floor($t %(60*60) ).'分钟后';
+        elseif ($t < 60 * 60 * 24 * 3)
+            $text = floor($t/(60*60*24))=== 1 ? '明天 ' . date('H:i', $time) : '后天 ' . date('H:i', $time) ;
+        elseif ($t < 60 * 60 * 24 * 30)
+            $text = date('m月d日 H:i', $time); //一个月以内
+        elseif ($t < 60 * 60 * 24 * 365)
+            $text = date('m月d日', $time); //一年以内
+        else
+            $text = date('Y年m月d日', $time); //一年以后
+
+    }
+
     return $text;
 }
 
